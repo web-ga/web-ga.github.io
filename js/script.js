2b457853254c3fbda8fa429fa8f94a08bdbe9e01
santoscarl <santos_cj28@yahoo.com>
2016-12-12 07:38:22 -0800

    webtech4

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $("nav a").css({"color" : "#fff"});
+      $(window).scroll(function() {
+        if ($(document).scrollTop() > 100) {
+         $("nav").css({"background-color":"#fff","padding":"0", "border-bottom" : "1px solid #eee" });
+         $("nav a").css({"color" : "#000"})
+         $(".glyphicon-menu-hamburger").css({"color" : "#000"});
+         $(".navbar-header a").css({"font-size" : "200%"});
+         } else{
+           $("nav").css({"background-color":"transparent", "padding":"1% 0", "border-bottom" : "none"});
+           $("nav a, .glyphicon-menu-hamburger").css({"color" : "#fff"});
+           $(".navbar-header a").css({"font-size" : "150%"});
+         }
+      });
+
+      $("#home-link, #about-link").on('click', function(event) {
+          if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 400, function(){
+              window.location.hash = hash;
+            });
+          }
+        });
+  $('.carousel').mouseenter(function(){
+      $('.glyphicon-chevron-right').addClass("animated fadeInRight");
+      $('.glyphicon-chevron-left').addClass("animated fadeInLeft");
+  });
+
+  });
