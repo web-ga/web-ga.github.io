cea3a7ee5e127e051b85b146875b8f7fa2231ac0
santoscarl <santos_cj28@yahoo.com>
2016-12-14 18:18:57 -0800

    update

--- js/script.js
+++ js/script.js
@@ -13,13 +13,13 @@ $(document).ready(function(){
          }
       });
 
-      $("#home-link, #about-link").on('click', function(event) {
+      $("#home-link, #about-link, #team-link, #services-link").on('click', function(event) {
           if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
               scrollTop: $(hash).offset().top
-            }, 500, function(){
+            }, 450, function(){
               window.location.hash = hash;
             });
           }
@@ -34,5 +34,39 @@ $(document).ready(function(){
       $('.about h2, .about p').addClass('animated fadeInUp');
   }, { offset: '50%' });
 
+  $('.img-carl, .img-jasper').css('opacity', 0);
+  $('.img-carl, .img-jasper').waypoint(function() {
+      $('.img-carl, .img-jasper').addClass('animated fadeInUp');
+  }, { offset: '80%' });
+
+  $('.about-jasper, .about-carl').css('opacity', 0);
+  $('.about-jasper, .about-carl').waypoint(function() {
+      $('.about-jasper, .about-carl').addClass('animated fadeInUp');
+  }, { offset: '80%' });
+
+  $('.team h2').css('opacity', 0);
+  $('.team h2').waypoint(function() {
+      $('.team h2').addClass('animated fadeInUp');
+  }, { offset: '80%' });
+
+  $('.services h2').css('opacity', 0);
+  $('.services h2').waypoint(function() {
+      $('.services h2').addClass('animated fadeInUp');
+  }, { offset: '80%' });
+
+  $('.services-icon1, .services-icon2, .services-icon3').css('opacity', 0);
+  $('.services-icon1, .services-icon2, .services-icon3').waypoint(function() {
+      $('.services-icon1, .services-icon2, .services-icon3').addClass('animated fadeInUp');
+  }, { offset: '80%' });
+
+  $('.about-jasper .a-toggle').click(function(){
+    $('.about-jasper .p-toggle').toggle("clip", 1000);
+    $('.about-jasper .a-toggle').toggleClass("fa-angle-double-up").toggleClass("fa-angle-double-down");
+  });
+
+  $('.about-carl .a-toggle').click(function(){
+    $('.about-carl .p-toggle').toggle("clip", 1000);
+    $('.about-carl .a-toggle').toggleClass("fa-angle-double-up").toggleClass("fa-angle-double-down");
+  });
 
   });
